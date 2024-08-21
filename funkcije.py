db2a66181acd8aff18a3043c7ee47cd0c9fc63ff
Hugo Trebše <ht15634@student.uni-lj.si>
2024-08-22 00:25:57 +0200

    Manjše spremembe in komentarji.

--- funkcije.py
+++ funkcije.py
@@ -6,8 +6,9 @@ meseci = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augus
 
 parent_path = r'C:\Users\hugot\Documents\FMF\1_letnik\UVP-projektna-naloga\podatkovna_baza'
 parent_path_shallow = r'C:\Users\hugot\Documents\FMF\1_letnik\UVP-projektna-naloga'
-    
-def sirote_str(str_file):
+
+#Loči html kodo na podnize, ki vsebujejo relevantne podatke za enega šahista.
+def sirote(str_file):
     sirote = re.split(r'bgcolor', str_file)
     if '<span class="article_separator">&nbsp;</span>' in str_file:
         odvecno = int(sirote[-1].index('<span class="article_separator">&nbsp;</span>') - 25)
@@ -17,6 +18,7 @@ def sirote_str(str_file):
     del sirote[0]
     return sirote
 
+#Iz niza, ki ga vrne funkcija sirote, izlušči podatke
 def dekompozicija(snippet):
     vzorec = r'&nbsp;([^<]+)</td><td>&nbsp;([^<]+)</td><td>&nbsp;([^<]+)</td><td>&nbsp;(\d+)</td><td>&nbsp;(\d+)</td><td>&nbsp;(\d+)'
     razdelitev = re.search(vzorec, snippet)
@@ -29,6 +31,7 @@ def dekompozicija(snippet):
         leto_rojstva = razdelitev.group(6)
     return ime, naziv, drzava, rating, st_iger, leto_rojstva
 
+#iz HMTL kode dobi datum
 def date_extractor(html):
     vzorec = r'contentheading" width="100%">\n\s*Top 100 Players (\w+) (\d+)'
     datum = re.search(vzorec, html)
@@ -39,7 +42,7 @@ def date_extractor(html):
     return f'{mesec}-{leto}'
 
 
-#nespremenljive podatke kot so ime, naziv, ter leto rojstva shranimo v en seperate .csv, podatke, ki so odvisni od časa, kot so rating, trenutni cas ter državljanstvo pa v .csv 
+#nespremenljive podatke kot so ime, naziv, ter leto rojstva shranimo v nek globalen .csv, podatke, ki so odvisni od časa, kot so rating, trenutni cas ter državljanstvo pa v .csv 
 #(drzavljanstvo je dejansko spremenljivo; pimer je Richard Rapoport, ki je prešel iz Madžarske v Romunsko šahovsko zvezo)
 
 #Ustvarimo funkcijo, ki bo nespremenljive podatke o sahistu shranila v neko globalno .csv datoteko.
@@ -51,10 +54,10 @@ def splosna_evidenca(ime, leto_rojstva, obstojeci_sahisti):
             pisalec.writerow([ime, leto_rojstva])
         with open(os.path.join(parent_path, str(ime)), 'w', newline='') as dat2:
             pass
-#nimam najmanjše ideje zakaj ime dodaja v teh čudnih oklepajih??? - zato ker bi čene v .csv-ju bili trije podatki - prvo ime in priimek namreč loči vejica.
+# Zakaj so vnosi v .csv datoteke oblike "priimek, ime",leto? Če ne bi bil podatek str(priimek, ime) v .csv formatu v resnici dva podatka.
 
-#Ustvarimo funkcijo, ki sprejme decomposed podatke o igralcu ter jih da v ustrezen .csv, oz ga ustvari ob primeru neobstoja.
-def pisatelj_csvjev(ime ,naziv, drzava, rating, st_iger, datum):
-    with open(os.path.join(parent_path, ime), 'a', newline='') as dat:
+#Ustvarimo funkcijo, ki sprejme podatke o igralcu (kot jih vrne funkcija dekompozicija) ter jih da v ustrezen .csv, oz ga ustvari ob primeru neobstoja.
+def pisatelj_csvjev(array, datum):
+    with open(os.path.join(parent_path, array[0]), 'a', newline='') as dat:
         pisalec = csv.writer(dat)
-        pisalec.writerow([datum, drzava, naziv, rating, st_iger])
+        pisalec.writerow([datum, array[2], array[1], array[3], array[4]])
--- main.py
+++ main.py
@@ -9,10 +9,9 @@ parent_html = r'https://ratings.fide.com/toparc.phtml?cod='
 for i in range(199):
     r = requests.get(f'{parent_html}{797-4*i}')
     vsebina = r.text
-    #v tej spremenljivki je HTML
-    posamezniki = funkcije.sirote_str(vsebina)
+    posamezniki = funkcije.sirote(vsebina)
     date = funkcije.date_extractor(vsebina)
     for posameznik in posamezniki:
         decomposed = funkcije.dekompozicija(posameznik)
         funkcije.splosna_evidenca(decomposed[0], decomposed[5], obstojeci_sahisti)
-        funkcije.pisatelj_csvjev(decomposed[0], decomposed[1], decomposed[2], decomposed[3], decomposed[4], date)
+        funkcije.pisatelj_csvjev(decomposed, date)
